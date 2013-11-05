11055c6a02291b7ad6d06b8161e6af36c337c374
Jiri Kratochvil <jiri.kratochvil@topmonks.com>
2013-11-05 09:30:47 +0100

    Implement serializable + added toString

--- perfmon-core/src/main/java/eu/kratochvil/perfmon/MonitorStats.java
+++ perfmon-core/src/main/java/eu/kratochvil/perfmon/MonitorStats.java
@@ -1,9 +1,13 @@
 package eu.kratochvil.perfmon;
 
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 /**
  * @author Jiri Kratochvil <jiri.kratochvil@topmonks.com>
  */
-public class MonitorStats {
+public class MonitorStats implements Serializable {
 
 	long totalCalls;
 
@@ -41,5 +45,13 @@ public class MonitorStats {
 
 	public long getShortest() {
 		return shortest;
+	}
+
+	@Override
+	public String toString() {
+		ToStringBuilder builder = new ToStringBuilder(this);
+		builder.append("totalCalls", totalCalls).append("totalTime", totalTime).append("avgTime", avgTime)
+		       .append("longest", longest).append("shortest", shortest);
+		return builder.toString();
 	}
 }
