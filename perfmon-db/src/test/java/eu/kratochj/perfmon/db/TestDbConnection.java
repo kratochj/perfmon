722f6bb11edd411517b818e501ea74e272710bb1
Jiri Kratochvil <jiri.kratochvil@topmonks.com>
2013-11-26 13:57:01 +0100

    ADD skeleton for database support

---
+++ perfmon-db/src/test/java/eu/kratochj/perfmon/db/TestDbConnection.java
@@ -0,0 +1,29 @@
+package eu.kratochj.perfmon.db;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Jiri Kratochvil <jiri.kratochvil@topmonks.com>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:testApplicationContext.xml")
+public class TestDbConnection {
+
+	@Autowired
+	QueryRunner query;
+
+	@Test
+	public void testSelectEmpty() throws Exception {
+		Statistics stats = query.query("select * from STATISTICS where rownum <= 1", new BeanHandler<Statistics>(Statistics.class));
+		assertNull(stats);
+
+	}
+}
