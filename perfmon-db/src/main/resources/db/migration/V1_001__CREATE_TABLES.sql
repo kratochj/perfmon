663bb3880112f122fac9ede7db5b9698e527f642
Jiri Kratochvil <jiri.kratochvil@topmonks.com>
2013-11-26 17:38:13 +0100

    MOD DDL scripts

--- perfmon-db/src/main/resources/db/migration/V1_001__CREATE_TABLES.sql
+++ perfmon-db/src/main/resources/db/migration/V1_001__CREATE_TABLES.sql
@@ -1,13 +1,13 @@
 create table STATISTICS (
    name varchar2(255) not null,
    category varchar2(255) not null,
-   dt_year number(2),
+   dt_year number(4),
    dt_month number(2),
    dt_day number(2),
    dt_hour number(2),
    last_updated date,
-   total_calls number(5),
-   total_time number(5),
+   total_calls number(10),
+   total_time number(10),
    longest number(10),
    shorttest number(10)
 );
